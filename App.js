5214933d67b8bcec92440c53e17b0a4c9867d3f8
Douglas <44684004+douglasvillegasvasquez@users.noreply.github.com>
2020-04-19 23:58:13 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,56 +1,13 @@
 import React,{useState} from 'react';
 import { StyleSheet, Text, View, TextInput,Button,FlatList } from 'react-native';
-import ContatoItem from './components/ContatoItem';
-import ContatoInput from './components/ContatoInput';
-export default function App() {
-  const [contatos, setContatos] = useState([]);
-  const [contadorContatos,setContadorContatos] = useState(10); //falta
+import TelaAdicionar from './telas/TelaAdicionar';
 
-  //para adicionar o que foi digitado
-  const adicionarContato = (contato,telefone)=> {
-    setContatos(contatos => {
-      console.log(contatos);
-      setContadorContatos(contadorContatos +2);
-      
-      return [{key:contadorContatos.toString(),valueCont:contato, valueTel:telefone}, ...contatos]
-      
-    })
-    
-  }
-/* (contato => {
-para cada contato rodar.
-})*/
-  const removerContato= (key) => {
-    setContatos (contatos => {
-      return contatos.filter(contato => {
-       return contato.key !==key;
-    
-      });
-      
-    });
-  };
+export default function App() {
+
+
   return (
 /*    <View style={styles.container} >*/
-<View style={styles.telaPrincipalView}>
-
-<ContatoInput onAdicionarContato = {adicionarContato} />
-<FlatList
-  data={contatos}
-  renderItem={
-    contato => (
-      <ContatoItem 
-      chave={contato.item.key}
-      contato={contato.item.valueCont}
-      telefone={contato.item.valueTel}
-      onDelete={removerContato}
-      />
-      
-    )
-  }
-  
-  
-/>
-</View>
+ <TelaAdicionar />
   );
 
 }
