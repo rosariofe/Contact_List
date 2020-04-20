0cd8b152ba434fefc13cfa95574cd0209d39cedf
Douglas <44684004+douglasvillegasvasquez@users.noreply.github.com>
2020-04-19 23:56:25 -0300

    Update ContatoItem.js

--- components/ContatoItem.js
+++ components/ContatoItem.js
@@ -1,64 +1,56 @@
 import React,{useState} from 'react';
 import {View,TextInput,Text,Button, StyleSheet, TouchableOpacity} from 'react-native'
- 
+import Titulo from '../components/Titulo'; 
+import cores from '../cores/cores';
 const ContatoItem = (props)=> {
     return (
         <TouchableOpacity 
         onLongPress={props.onDelete.bind(this, props.chave, props.chave2)}>
         <View style = { styles.itemNaLista}>
         <View style = { styles.Titulo}>
-        <View style = { styles.Inicial}>
-        <Text style ={{color:'#FFFAFA', fontSize:18, marginLeft:16}}> 
-            {props.contato.substring(0,1)}
-        </Text>
-        </View>
-        <Text style ={{color:'#FFFAFA', fontSize:18, textAlign:"center"}}> 
+
+        <Text style ={styles.contato}> 
        
         {props.contato}
           </Text>
         </View>
-        <Text style={{color:'#000',fontSize:10}}>  Celular</Text>
-        <Text  style = { styles.Nome}
         
+        <Text  style = { styles.Nome}
+
         >
           {props.telefone} </Text>
-        
+          <Text style={{color:'#000',fontSize:10}}>  Celular</Text>       
         </View>
         </TouchableOpacity>
     )
 }
 const styles = StyleSheet.create({
-    itemNaLista:{
-        paddingTop: 20,
-        paddingBottom:10,
-        padding: 12,
-        backgroundColor: 'white',
-        borderBottomColor: '#000',
-        borderWidth: 1,
-        marginBottom:60,
-        borderRadius: 3
-    },
+   contato:{color:'#FFFAFA',
+    fontSize:18,
+    marginLeft:16
+},
     Titulo:{
-        paddingTop: 50,
+        paddingTop: 190,
         paddingBottom: 10,
-        backgroundColor: '#1E90FF',
+        backgroundColor: cores.fundo,
         marginLeft:-12,
         marginRight:-12,
         marginTop:-20,
         marginBottom:10,        
-        borderRadius: 3
+        borderRadius: 3,
+        
        
     },
-    Inicial:{
-        paddingTop: 10,
-        paddingBottom: 10,
-        backgroundColor: '#000033',
-        marginLeft:105,
-        marginRight:108,
-        marginTop:2,
-        marginBottom:10,        
-        borderRadius: 60
-       
+    itemNaLista:{
+        height:345,
+        paddingTop: 20,
+        paddingBottom:10,
+        padding: 12,
+        backgroundColor: cores.branco,
+        borderBottomColor: '#000',
+        borderWidth: 1,
+        marginBottom:20,
+        borderRadius: 3
     },
     Nome:{
         paddingTop:6,
